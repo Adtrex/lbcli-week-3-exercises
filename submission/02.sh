15c84991b04bd4532e9ee01010876f9da4697e58
adtrex <toluadekunte@gmail.com>
2025-04-03 12:46:39 +0100

    sh 02

--- submission/02.sh
+++ submission/02.sh
@@ -1,1 +1,6 @@
 # Create a native segwit address and get the public key from the address.
+
+address=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress)
+
+bitcoin-cli -regtest -rpcwallet=btrustwallet getaddressinfo "$address" | jq -r ".pubkey"
+
